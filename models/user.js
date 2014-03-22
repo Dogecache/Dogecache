275e9ddd0b4c0c8960fe02eb2f8bc2af3d69ab44
Matthew Hsu <gears88@gmail.com>
2014-03-22 19:47:09 -0400

    [new] added dogeaddress initialization to user.js

--- models/user.js
+++ models/user.js
@@ -1,24 +1,34 @@
 var mongoose = require('mongoose');
 
+var dogeAPI = requrie('libraries/dogeapi')
+doge = new dogeAPI();
+
 var userSchema = new mongoose.Schema({
     fbId: Number,
     displayName: String,
     dogeAddress: String
 });
 
-userSchema.statics.findOrCreate = function(profile, callback) {
+userSchema.statics.findOrCreate = function (profile, callback) {
     var that = this;
+    var user;
     // try to check if user already exists
-    that.findOne({fbId: profile.id}, function(err, result) {
+    that.findOne({fbId: profile.id}, function (err, result) {
         if (!err && result) {
             callback(null, result);
         } else {
-            // create new user
-            var user = new that({fbId: profile.id, displayName: profile.displayName});
+            //initialize dogeaddress
+            doge.createUser(profile.id, function (error, res) {
+                if (error) {
+                    // @TODO: Handle error
+                }
+                var paymentAddress = res.data.address;
+                // create new user
+                user = new that({fbId: profile.id, displayName: profile.displayName, dogeAddress: paymentAddress});
+            });
 
-            // TODO: initialize dogeAddress
 
-            user.save(function() {
+            user.save(function () {
                 if (err) callback(err);
                 callback(null, user);
             });
