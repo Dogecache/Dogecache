5a14e7b708f0ee3d30ff57c26dcada9101039d55
Zachary Liu <liu.zachary@gmail.com>
2014-03-22 21:26:57 -0400

    Allow API authentication via fbId

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 var Cache = require('../models/cache');
+var User = require('../models/user');
+var passport = require('passport');
 
 exports.cache = function(req, res) {
+    if (!req.user) {
+        // TODO: more secure API key login method
+        User.findOne({fbId: req.body.fbId}, function(err, user) {
+            if (!err) {
+                req.login(user);
+            }
+        })
+    }
+
     // First, add the cache
     Cache.addCache(req.user, req.body.amount, req.body.longitude, req.body.latitude, function(err, cache) {
         if (err) {
