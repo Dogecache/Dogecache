4c7451dd8e31b8fbc13f2725239415a5e8bd759c
Austin Eng <213reeses@gmail.com>
2014-03-23 03:51:11 -0400

    move notify msg

--- public/js/map.js
+++ public/js/map.js
@@ -23,9 +23,6 @@
                 $('.search-area').css("color", "rgba(255,255,255, 0)");
 
                 //PUT STUFF HERE FOR WHEN USER SUCCESSFULLY SEARCHES
-                if (parseInt($('#wager-slider').val())>parseInt($('#balance').text())) {
-                    notify('Insufficient Doge', 'Please deposit more dogecoin.');
-                }
             }
         });
 
@@ -89,6 +86,9 @@
                 that.$area.css("color", "rgba(255,255,255, 0)");
 
                 //PUT STUFF HERE FOR WHEN USER SUCCESSFULLY SEARCHES
+                if (parseInt($('#wager-slider').val())>parseInt($('#balance').text())) {
+                    notify('Insufficient Doge', 'Please deposit more dogecoin.');
+                }
 
             }
         });
