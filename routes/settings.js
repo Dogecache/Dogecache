d476fc5095df99ff8d8b6fff42a212da0ec80e7c
Matthew Hsu <gears88@gmail.com>
2014-03-23 00:45:13 -0400

    [new] added google charts qr code generator

--- routes/settings.js
+++ routes/settings.js
@@ -1,10 +1,12 @@
 /*
  * GET settings page.
  */
+var googlecharts = require('../libraries/googlechartsapi');
 
 exports.index = function(req, res){
   res.render('settings', {
     title: 'Settings',
-    user: req.user
+    user: req.user,
+    qr: googlecharts.qr(600,600,req.user.dogeAddress)
   });
 };
