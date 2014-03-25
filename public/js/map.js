e318a9fd03f93cad9a8b20ebb06fc185349d0ff4
Austin <213reeses@gmail.com>
2014-03-24 20:15:29 -0400

    Revert "change gps wait to modal"
    This reverts commit b04ed72bc51a79b6c879b307e0c79b280a107fbe.
    
    Conflicts:
    	public/js/map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,5 @@
 (function () {
     var map, balance, searchSlider;
-    var gpsPermissions=false;
 
     $(document).ready(function () {
         map = new Map('map', function() {
@@ -17,17 +16,19 @@
         });
         var searchSlider = new SearchSlider('#search-slider', '#search-drop', '.search-area');
         balance = new Balance(startingBalance, '#balance_num');
-        setTimeout(function(){
-            if (!gpsPermissions) {
-                notify("Waiting for GPS permissions...");
-            }
-        }, 200)
+
         navigator.geolocation.getCurrentPosition(gpsPermissionGranted, function(err) {console.log(err)}, {enableHighAccuracy: true});
     });
 
     function gpsPermissionGranted(position) {
-        gpsPermissions=true;
-        closeNotify();
+        $('#gpsApproval h1').html('<i class="fa fa-thumbs-o-up"></i>');
+        $('#gpsApproval').animate({
+            backgroundColor: '#27ae60'
+        }, 300).delay(700).animate({
+            opacity: 0
+        }, 200, function () {
+            $('#gpsApproval').css('zIndex', '-1')
+        });
         console.log(position.coords.latitude + ', ' + position.coords.longitude);
 
         map.init(position);
