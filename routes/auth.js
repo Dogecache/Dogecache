6df43953acb1fe3c12b2256929afe396c7ae4a77
Zachary Liu <liu.zachary@gmail.com>
2014-04-17 16:51:42 -1000

    Build callbackURL based on config url

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ var config = require('../config');
 passport.use(new FacebookStrategy({
     clientID: config.facebook_clientid,
     clientSecret: config.facebook_clientsecret,
-    callbackURL: (process.env.NODE_ENV == 'production') ? 'http://www.dogecache.com/auth/callback' : 'http://localhost:3000/auth/callback' //@TODO possibly move to config
+    callbackURL: config.url + '/auth/callback'
 }, function(accessToken, refreshToken, profile, done) {
     User.findOrCreate(profile, function(err, user) {
         done(null, user);
