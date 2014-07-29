cd45f0148f40425abc4526d8eb75b9893dedd648
Zachary Liu <liu.zachary@gmail.com>
2014-07-29 10:45:15 -0400

    Fix doge.moveToUser response format (reverted from commit 57f6d2557bccea762f82898db5654746b6c53347)

--- dogeapi.js
+++ dogeapi.js
@@ -62,8 +62,9 @@ DogeAPI.prototype.moveToUser = function(toUserID, fromUserID, amount, callback) 
         'pin': this.secret_pin
     }, function(err, resp) {
         if (!err && resp.status == "success") {
-            var fee = parseFloat(resp.data.network_fee) + parseFloat(resp.data.blockio_fee);
-            return callback(null, fee);
+            resp.data.success = {};
+            resp.data.success.fee = parseFloat(resp.data.network_fee) + parseFloat(resp.data.blockio_fee);
+            return callback(null, JSON.stringify(resp));
         } else {
             return callback(resp);
         }
