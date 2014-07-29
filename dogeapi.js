b95f5d8f7031138c660d7fe5baad60dd72752b50
Zachary Liu <liu.zachary@gmail.com>
2014-07-29 08:52:19 -0400

    Preliminary DogeAPI->BlockIO shim

--- dogeapi.js
+++ dogeapi.js
@@ -1,8 +1,97 @@
 "use strict";
 
-var dogeAPI = require('dogeapi');
 var config = require('./config');
-module.exports = new dogeAPI({
-    endpoint: 'https://dogeapi.com/',
-    apikey: config.setup.dogeapiKey
-});
+
+var request = require('request');
+var validator = require('validator');
+
+var BlockIo = require('block_io');
+
+function DogeAPI(api_key, secret_pin) {
+    this.block_io = new BlockIo(api_key);
+    this.secret_pin = secret_pin;
+    this.user_ids = {};
+}
+
+DogeAPI.prototype.createUser = function(userID, callback) {
+    var self = this;
+    if(!userID) return callback('Missing user id.');
+    if(!validator.isAlphanumeric(userID)) return callback('Invalid user id.');
+
+    this.block_io.create_user({'label': userID}, function(err, resp) {
+        if (!err) {
+            self.user_ids[userID] = resp.data.user_id;
+            return callback(null, JSON.stringify(resp));
+        } else {
+            return callback(resp);
+        }
+    });
+};
+
+DogeAPI.prototype.withdrawFromUser = function(userID, paymentAddress, amount, pin, callback) {
+    var self = this;
+    if(!userID) return callback('Missing user id.');
+    if(!paymentAddress) return callback('Missing payment address.');
+    if(!amount) return callback('Missing amount to withdraw.');
+    if(!validator.isAlphanumeric(userID)) return callback('Invalid user id.');
+
+    this.block_io.withdraw_from_user({
+        'amount': amount,
+        'payment_address': paymentAddress,
+        'from_user_ids': this.user_ids[userID].toString(),
+        'pin': this.secret_pin
+    }, function(err, resp) {
+        if (!err) {
+            return callback(null, JSON.stringify(resp));
+        } else {
+            return callback(resp);
+        }
+    });
+};
+
+DogeAPI.prototype.moveToUser = function(toUserID, fromUserID, amount, callback) {
+    var self = this;
+    if(!toUserID) return callback('Missing user id to send to.');
+    if(!fromUserID) return callback('Missing user id to send from.');
+    if(!amount) return callback('Missing amount to move.');
+
+    this.block_io.withdraw_from_user({
+        'amount': amount,
+        'from_user_ids': this.user_ids[fromUserID].toString(),
+        'to_user_id': this.user_ids[toUserID].toString(),
+        'pin': this.secret_pin
+    }, function(err, resp) {
+        if (!err) {
+            resp.data.success = {};
+            resp.data.success.fee = parseFloat(resp.data.network_fee) + parseFloat(resp.data.blockio_fee);
+            return callback(null, JSON.stringify(resp));
+        } else {
+            return callback(resp);
+        }
+    });
+};
+
+DogeAPI.prototype.getUsers = function(callback) {
+    var self = this;
+
+    this.block_io.get_users(function(err, resp) {
+        if (!err) {
+            var new_resp = {"data": {"users": []}};
+            for (var i=0; i<resp.data.addresses.length; i++) {
+                var user = resp.data.addresses[i];
+                var new_user = {
+                    'user_id': user.label,
+                    'payment_address': user.address,
+                    'user_balance': user.available_balance
+                };
+                new_resp.data.users.push(new_user);
+                self.user_ids[user.label] = user.user_id;
+            }
+            return callback(null, JSON.stringify(new_resp));
+        } else {
+            return callback(resp);
+        }
+    });
+};
+
+module.exports = new DogeAPI(config.setup.blockio_key, config.setup.blockio_pin);
